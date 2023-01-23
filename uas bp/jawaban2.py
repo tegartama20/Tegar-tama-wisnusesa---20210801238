977f5079a66e887c2a0c62a4e68574c77638a058
wisnu <114716990+tegartama20@users.noreply.github.com>
2023-01-23 19:04:25 +0700

    Create jawaban2.py

---
+++ uas bp/jawaban2.py
@@ -0,0 +1,10 @@
+Blok try akan menghasilkan NameError, karena x tidak ditentukan:
+
+try:
+  print(x)
+except NameError:
+  print("Variable x tidak ditentukan")
+except:
+  print("Telah terjadi kesalahan")
+  
+  
