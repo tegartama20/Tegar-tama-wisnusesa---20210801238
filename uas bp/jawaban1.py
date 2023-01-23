64eac270f55c2e7f48e498b6066b1c6ab88f59c0
wisnu <114716990+tegartama20@users.noreply.github.com>
2023-01-23 19:02:23 +0700

    Create jawaban1.py

---
+++ uas bp/jawaban1.py
@@ -0,0 +1,12 @@
+def fungsi_satu():
+    print('Ini adalah saya anak pertamaa....');
+
+def fungsi_kedua():
+    print('Ini adalah nadia anak kedua......');
+
+def fungsi_ketiga():
+    print('Ini adalah naura anak ketiga.....');
+    
+fungsi_satu()
+fungsi_kedua()
+fungsi_ketiga()
